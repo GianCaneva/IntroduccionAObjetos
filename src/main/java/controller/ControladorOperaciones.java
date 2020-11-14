6107eaa449be3430e7a947f138bee0665bd5143d
Gianfranco Caneva <gfocaneva@gmail.com>
2020-11-14 12:35:02 -0300

    controller Operaciones first steps

---
+++ src/main/java/controller/ControladorOperaciones.java
@@ -0,0 +1,28 @@
+package controller;
+
+public class ControladorOperaciones {
+
+
+    public ControladorOperaciones() {
+
+    }
+
+    private int calcularComisionesCheques(){
+
+    }
+    private int calcularCantidadOpMonetizadas(){}
+
+    private int calcularValorPromedioTasa(){
+    }
+
+    private int calcularPorcentajeComision(){
+    }
+    private int calcularSaldoMora(){
+    }
+    private int calcularConsolidada(){
+    }
+
+
+
+    
+}
