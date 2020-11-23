13cb9ff1af408f780d3e634861129b3a6b6ca5b5
Gianfranco Caneva <gfocaneva@gmail.com>
2020-11-23 16:48:34 -0300

    Cambio en accionistas

--- src/main/java/dto/Empresa/SocioParticipe.java
+++ src/main/java/dto/Empresa/SocioParticipe.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class SocioParticipe extends Empresa {
 
